079770ed76202c91a2d05b0579145b1a912cf210
Maine679 <maine679@gmail.com>
2021-05-24 22:18:23 +0300

    Обучение composer и умение пользоваться готовыми компонентами.

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use app\MyQB;
 
 $example = new MyQB();
 
+
+
 //$results = $example->FindOne('users',['*'],14);
 //$results = $example->getAll('users',['*']);
 //$results = $example->Insert('users',['name'=>'name','surname'=>'surname','id'=>'233']);
